389505ad679292f50f95857925b4a450dce056c2
stan <chenhuixiang5319@gmail.com>
2014-12-18 06:48:59 -0500

    Client program parameter: parameter1 : IP/port parameter2: sendMode, 0 for testing API mode, 1 for testing throughput, default is 1 parameter3: T, for testing throughput mode, 12.5 indicates that send throughput is 8000/s, default is 12.5 parameter4: num of users in this client, default is 100000 parameter5: first user ID in this client, default is 0 parameter6: num of total users for the server, default is 100000

--- run_client.sh
+++ run_client.sh
@@ -3,4 +3,14 @@
 memory=$1
 echo -J-Xmx"$memory" -J-Xms"$memory"
 echo "./run_client.sh memSize serverIP:port sendMode T"
-sbt -J-Xmx"$memory" -J-Xms"$memory" "run 10.227.56.44:8080 0 0 10.0"
+sbt -J-Xmx"$memory" -J-Xms"$memory" "run 10.227.56.44:8080 0 10.0"
+
+#parameter for testing RESTAPI
+#sbt -J-Xmx"$memory" -J-Xms"$memory" "run 192.168.1.5:9056 0 10.0"
+
+#parameter for testing single machine throughput
+#sbt -J-Xmx"$memory" -J-Xms"$memory" "run 192.168.1.5:9056 1 12.5"
+
+#parameter for testing two-machine throughput
+#sbt -J-Xmx"$memory" -J-Xms"$memory" "run 192.168.1.5:9065 0 1 12.5 50000 0 100000"
+#sbt -J-Xmx"$memory" -J-Xms"$memory" "run 192.168.1.5:9065 0 1 12.5 50000 50000 100000"
