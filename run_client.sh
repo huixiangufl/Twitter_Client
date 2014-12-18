a395ae7e5426bf15c82b780b0fcede6ee2530f41
stan <chenhuixiang5319@gmail.com>
2014-12-18 00:54:58 -0500

    Version 10 added parameter SendTweet ViewHomeTimeline ViewUserTimeline GetFriends GetFollowers CreateFriendship DestroyFriendship DestroyTweet ForwardTweet inside ViewHomeTimeline MentionTimeline

--- run_client.sh
+++ run_client.sh
@@ -3,4 +3,4 @@
 memory=$1
 echo -J-Xmx"$memory" -J-Xms"$memory"
 echo "./run_client.sh memSize serverIP:port testAPImode sendMode T"
-sbt -J-Xmx"$memory" -J-Xms"$memory" "run $2 $3 $4 $5"
+sbt -J-Xmx"$memory" -J-Xms"$memory" "run $2 $3 $4 $5 $6 $7 $8"
