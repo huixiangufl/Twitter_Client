51e9591a356dff47a4e3fc26a2b5cea3415ec608
stan <chenhuixiang5319@gmail.com>
2014-12-17 23:20:58 -0500

    Version 9: test the throughput of sending tweets SendTweet ViewHomeTimeline ViewUserTimeline GetFriends GetFollowers CreateFriendship DestroyFriendship DestroyTweet ForwardTweet inside ViewHomeTimeline MentionTimeline

--- src/main/scala/twitterclient/twitterclient.scala
+++ src/main/scala/twitterclient/twitterclient.scala
@@ -80,7 +80,7 @@ object twitterclient extends App {
   val numTotalUsers: Int = 100000
   var numOfFollowers: ArrayBuffer[Int] = new ArrayBuffer
   var maxNumOfFollowers = 5000
-  var T = 12.5
+  var T = 8.0
   var simulateOption = 1 // option 0: our send mode; option 1: junyun mode
 
   var serverIP: String = "192.168.1.3:9056" //"10.244.33.189:8080" //"10.227.56.44:8080"
